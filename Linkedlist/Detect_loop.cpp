05da9613553d27956cc860d53b900d9a421eb5e9
shag527 <shagunkatoch527@gmail.com>
2020-09-23 22:06:37 +0530

    Detect loop in a linkedlist

---
+++ Linkedlist/Detect_loop.cpp
@@ -0,0 +1,76 @@
+// Program to reverse linked list in groups of k
+
+#include<bits/stdc++.h>
+using namespace std;
+#define f(x,n,d) for(int i=x;i<n;i+=d)
+class linkedlist{
+    int val;
+    linkedlist *next;
+    public:
+    linkedlist *add_node(linkedlist *root,int val)
+    {
+        if(root==NULL)
+        {
+            root=new linkedlist;
+            root->val=val;
+            root->next=NULL;
+        }
+        else
+        {
+            linkedlist *ptr=root;
+            if(ptr->next==NULL)
+            {
+                linkedlist *temp=new linkedlist;
+                temp->val=val;
+                temp->next=NULL;
+                ptr->next=temp;
+                return root;
+            }
+            while(ptr->next!=NULL)
+            ptr=ptr->next;
+            linkedlist *temp=new linkedlist;
+            temp->val=val;
+            temp->next=NULL;
+            ptr->next=temp;
+        }
+        return root;
+    }
+    bool detect_loop(linkedlist *root)
+    {
+        linkedlist *slow=root,*fast=root;
+        while(fast->next!=NULL&&slow!=NULL)
+        {
+            slow=slow->next;
+            fast=fast->next->next;
+            if(slow==fast)
+            return true;
+        }
+        return false;
+    }
+    void show_list(linkedlist *root)
+    {
+        linkedlist *ptr=root;
+        while(ptr!=NULL)
+        {
+            cout<<ptr->val<<" ";
+            ptr=ptr->next;
+        }
+        cout<<endl;
+    }
+};
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    linkedlist *root=NULL;
+    int num,val;
+    cin>>num;
+    while(num--)
+    {
+        cin>>val;
+        root=root->add_node(root,val);
+    }
+    cout<<root->detect_loop(root)<<endl;
+    root->show_list(root);
+    return 0;
+}
